7d9443b5a9ad9efbf74289dee131162bbcab5e88
박성현 <bmanerdaniel@gmail.com>
2021-04-29 12:43:20 +0900

    Resolve the Error and Read the data from API

--- src/Movie.js
+++ src/Movie.js
@@ -1,25 +1,35 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css";
 
-function Movie(id, year, title, summary, poster){
-    return (
-        <div class="movie">
-            <img src={poster} alt={title} title={title}/>
-            <div class="movie__data">
-                <h3 class="movie__title">{title}</h3>
-                <h5 class="movie__year">{year}</h5>
-                <p class="movie__summary">{summary}</p>
-            </div>
-        </div>
-    );
+function Movie({ id, year, title, summary, poster, genres }) {
+  //parameter problem
+  return (
+    <div className="movie">
+      <img src={poster} alt={title} title={title} />
+      <div className="movie__data">
+        <h3 className="movie__title">{title}</h3>
+        <h5 className="movie__year">{year}</h5>
+        <ul className="genres">
+          {genres.map((genre, index) => (
+            <li key={index} className="genres__genre">
+              {genre}
+            </li>
+          ))}
+          <p className="movie__summary">{summary}</p>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 Movie.propTypes = {
-    id: PropTypes.number.isRequired,
-    year: PropTypes.number.isRequired,
-    title: PropTypes.string.isRequired,
-    summary: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
+  year: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default Movie;
